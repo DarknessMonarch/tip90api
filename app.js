c15a4b5c7eb78ef01b5483037f2dbad654d72ffe
zero-stealth <penguincipher@gmail.com>
2025-03-25 05:43:55 +0300

    api stable

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const corsOptions = {
 };
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(helmet());
